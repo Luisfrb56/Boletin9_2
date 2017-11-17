616b74927c13e4e9a6004f0dc196f413ca540775
adm <adm@LAPTOP-EQL1FQIM>
2017-11-17 11:31:35 +0100

    Boletin9_2

---
+++ src/boletin9_2/Boletin9_2.java
@@ -0,0 +1,13 @@
+
+package boletin9_2;
+
+
+public class Boletin9_2 {
+
+   
+    public static void main(String[] args) {
+      Metodos2 obj=new Metodos2();
+      obj.calcularMet();
+    }
+    
+}
